009baf79f551c204a827ed0472c0ee5111ab1181
Aime Ngongang <aimechicago5@yahoo.fr>
2013-03-09 13:13:54 -0800

    added a lot of files

--- web_project/website/urls.py
+++ web_project/website/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls.defaults import patterns, include,url
-
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+#..rest of url.py config...
+urlpatterns += staticfiles_urlpatterns()
 urlpatterns = patterns('website.views',
     url(r'^add','add')
     )
