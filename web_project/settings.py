381cdad6e5c0c18b6fd13c2b9e3dc7197fda36e8
Aime Ngongang <aimechicago5@yahoo.fr>
2013-03-09 16:17:18 -0800

    changed add.html

--- web_project/settings.py
+++ web_project/settings.py
@@ -70,7 +70,7 @@ STATIC_URL = '/static/'
 ADMIN_MEDIA_PREFIX = '/static/admin/'
 
 # Additional locations of static files
-STATICFILES_DIRS = (
+STATICFILES_DIRS = (~/My_django_files/heroku_web/web_project/website/static,
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
