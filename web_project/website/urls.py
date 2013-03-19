a04679d056c8f63afe55c97f3fcb484d6f27171f
Aime Ngongang <aimechicago5@yahoo.fr>
2013-03-18 20:14:29 -0700

    cleaning up

--- web_project/website/urls.py
+++ web_project/website/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls.defaults import patterns, include,url
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 #..rest of url.py config...
-urlpatterns += patterns('website.views',
+urlpatterns = patterns('website.views',
     url(r'^add','add')
     )
